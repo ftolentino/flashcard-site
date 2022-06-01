46f44efbcbd549b02b9034985248d467c9d9a61c
ftolentino <filmer.tolentino@gmail.com>
2022-06-01 15:36:33 -0700

    USE jQuery to make flash card that toggle show/hide on click
    Co-authored-by: Matt Herbert <matthew.f.herbert@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  $(".card1").click(function() {
+    $(".answer1").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".card2").click(function() {
+    $(".answer2").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".card3").click(function() {
+    $(".answer3").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".card4").click(function() {
+    $(".answer4").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".card5").click(function() {
+    $(".answer5").toggle();
+  });
+});
